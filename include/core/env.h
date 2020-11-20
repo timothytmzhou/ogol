ea35da312c417c20360ab163e236afa5f5deeca5
timothy <timothytmzhou@gmail.com>
2020-11-19 23:38:02 -0600

    Add enviroment structure

---
+++ include/core/env.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <core/s_expr.h>
+#include <map>
+#include <memory>
+#include <string>
+
+using std::string;
+using std::unique_ptr;
+using std::map;
+
+namespace ogol::core {
+
+/**
+ * Stores an environment, a mapping between names and procedures/s-expressions.
+ */
+class Env {
+public:
+  Env(unique_ptr<Env> parent_, map<string, SExpr> names);
+  SExpr & operator[] (const Token& identifier_token);
+private:
+  unique_ptr<Env> parent_;
+  map<string, SExpr> names_;
+};
+
+
+} // namespace ogol::core
---
+++ src/core/env.cc
@@ -0,0 +1,22 @@
+#include "core/env.h"
+#include "core/exception.h"
+
+#include <utility>
+
+namespace ogol::core {
+
+Env::Env(unique_ptr<Env> parent, map<string, SExpr> names)
+    : parent_(std::move(parent)), names_(std::move(names)) {}
+
+SExpr &Env::operator[](const Token &identifier_token) {
+  if (names_.find(identifier_token.value) == names_.end()) {
+    if (parent_) {
+      return (*parent_)[identifier_token];
+    } else {
+      throw UndefinedNameError("Could not find ", identifier_token.line_num);
+    }
+  }
+  return names_.at(identifier_token.value);
+}
+
+} // namespace ogol::core
