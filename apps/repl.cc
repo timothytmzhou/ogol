ae32a3f0cb4e795e208a64f62cfa62e483ce010b
timothy <timothytmzhou@gmail.com>
2020-11-20 06:49:03 -0600

    Add REPL for evaluation

---
+++ apps/repl.cc
@@ -0,0 +1,27 @@
+#include "core/env.h"
+#include "core/lexer.h"
+#include "core/parser.h"
+#include "core/procedures.h"
+#include "core/s_expr.h"
+#include <iostream>
+
+using ogol::core::Atom;
+
+int main() {
+  static ogol::core::Env main_env(nullptr,
+                                  map<string, ogol::core::SExpr>{
+                                      {"print", Atom(ogol::core::Print)},
+                                      {"define", Atom(ogol::core::Define)},
+                                      {"+", Atom(ogol::core::Add)},
+                                      {"-", Atom(ogol::core::Sub)},
+                                      {"*", Atom(ogol::core::Mul)},
+                                      {"/", Atom(ogol::core::Div)},
+                                  });
+
+  ogol::core::Lexer lexer;
+  "(print 'hello world')" >> lexer;
+  std::queue<ogol::core::Token> tokens = lexer.tokenize();
+  ogol::core::Parser parser(tokens);
+  parser.parse().Eval(main_env);
+  return 0;
+}
