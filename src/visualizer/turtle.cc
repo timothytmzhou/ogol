86bbdd5741b9a359391e7df7b1b33cadcc56d21a
timothy <timothytmzhou@gmail.com>
2020-12-04 14:13:35 -0600

    Write implementation for Turtle class

---
+++ src/visualizer/turtle.cc
@@ -0,0 +1,29 @@
+#include "visualizer/turtle.h"
+
+namespace ogol::visualizer {
+
+Turtle::Turtle(const dvec2 &position, const dvec2 &forward, double speed,
+               size_t max_x, size_t max_y)
+    : position_(position), forward_(forward), speed_(speed), max_x_(max_x),
+      max_y_(max_y) {}
+
+ivec2 Turtle::GetPosition() { return position_; }
+
+ci::Color Turtle::GetColor() { return color_; }
+
+void Turtle::Update() {
+  position_ += speed_ * forward_;
+  // clamp position within range
+  position_.x = std::clamp(position_.x, -(double)max_x_, (double)max_x_);
+  position_.y = std::clamp(position_.y, -(double)max_y_, (double)max_y_);
+}
+
+void Turtle::Rotate(double theta) {
+  forward_ = glm::rotate(forward_, glm::radians(theta));
+}
+
+void Turtle::SetColor(const ci::Color &color) { color_ = color; }
+
+void Turtle::SetSpeed(double speed) { speed_ = speed; }
+
+} // namespace ogol::visualizer
